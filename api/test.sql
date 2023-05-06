00f3532d9dbc94b1edb8626278754de70f0d7710
Vaibhav Venkat <vibovenkat@gmail.com>
2023-05-06 18:24:47 -0500

    feat: create more graph queries

--- api/test.sql
+++ api/test.sql
@@ -1,4 +1,3 @@
--- NoSQL-esque modeling, schemaless
 DELETE food;
 CREATE food:pasta SET toppings=['basil'], rating=9;
 CREATE food:pizza SET toppings=['cheese'], rating=10;
@@ -7,7 +6,27 @@ CREATE food:pizza SET toppings=['cheese'], rating=10;
 SELECT * FROM food WHERE rating > 9;
 SELECT * FROM food WHERE toppings CONTAINS 'basil';
 -- update array
-UPDATE food SET toppings += ["pineapple"] WHERE id = "food:pasta"; -- :)
+UPDATE food SET toppings += ["pineapple"] WHERE id = "food:pizza"; -- :)
 
 SELECT * FROM food;
--- SQL schema similar to postgres
+
+-- one to one
+UPDATE food:pizza SET side = food:pasta;
+
+SELECT side.rating, side.toppings FROM food:pizza;
+
+-- one to many
+CREATE drink:coke
+SET name="Coca Cola", soda=true, price = 2, rating = 7;
+
+CREATE drink:lemonade
+SET name="Lemonade", soda=false, price = 1, rating = 9;
+
+UPDATE food:pasta SET drinks=[drink:lemonade, drink:coke];
+
+-- Many to Many
+-- graph-esque
+RELATE food:pizza->went_well_with->drink:lemonade;
+
+SELECT ->went_well_with->drink FROM food;
+SELECT <-went_well_with<-food FROM drink;
